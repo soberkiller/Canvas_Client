9b6d618a8006e2b6e83e1da692ac0a1d79025ff3
Dov Kruger <Dov.Kruger@gmail.com>
2018-04-11 12:31:07 -0400

    added example pulling out property from conf file

--- zfm/src/com/killer/connecting/ReadConfig.java
+++ zfm/src/com/killer/connecting/ReadConfig.java
@@ -5,5 +5,7 @@ public class ReadConfig {
 	public static void main(String[] args) {
 		Properties p = new Properties();
 		p.load("conf/canvasclient.conf");
-		
+		String token = p.getProperty("token");
+		System.out.println(token);
+		p.close();
 }
