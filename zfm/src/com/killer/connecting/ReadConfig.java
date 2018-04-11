643edcdd34ddc94297406e7e56ebe0316eee9a79
Dov Kruger <Dov.Kruger@gmail.com>
2018-04-11 12:26:17 -0400

    added sample conf file with Read. conf directory is in .gitignore so that security is not compromised

---
+++ zfm/src/com/killer/connecting/ReadConfig.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class ReadConfig {
+	public static void main(String[] args) {
+		Properties p = new Properties();
+		p.load("conf/canvasclient.conf");
+		
+}
